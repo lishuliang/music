0333155808b0799b7014622b2f8360def53aa134
Li Shuliang <532784460@qq.com>
2018-08-30 19:25:07 +0800

    recommend page

--- src/main.js
+++ src/main.js
@@ -6,9 +6,13 @@ import App from './App'
 import fastclick from 'fastclick'
 import router from './router'
 import './common/stylus/index.styl'
+import VueLazyLoad from 'vue-lazyload'
 
 
 fastclick.attach(document.body)
+Vue.use(VueLazyLoad,{
+  loading:require('./common/image/default.png')
+})
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
